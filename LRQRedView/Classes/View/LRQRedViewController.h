bb07bf67f1a9206efccb239e1b3465665aab817f
李仁强 <leonard.li@zhan.com>
2018-03-23 15:51:59 +0800

    add viewcontroller

---
+++ LRQRedView/Classes/View/LRQRedViewController.h
@@ -0,0 +1,12 @@
+//
+//  LRQRedViewController.h
+//  LRQRedView
+//
+//  Created by lirenqiang on 23/03/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRQRedViewController : UIViewController
+
+@end
